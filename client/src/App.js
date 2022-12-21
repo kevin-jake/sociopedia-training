3f92218fc2cf50dc10d9bf61a9438fe419de4d49
Kevin Jake <kevin_jake_04@yahoo.com>
2022-12-21 10:22:17 +0800

    add user login register

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/home" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
             <Route path="/profile/:userId" element={<ProfilePage />} />
           </Routes>
         </ThemeProvider>
