933f9a3ea5a853fb3a8685f2b24fefbfc099dd2b
Kevin Jake <kevin_jake_04@yahoo.com>
2022-12-23 09:29:23 +0800

    add posts widgets

--- client/src/scenes/homePage/HomePage.jsx
+++ client/src/scenes/homePage/HomePage.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import Navbar from 'scenes/navbar/Navbar'
 import UserWidget from 'scenes/widgets/UserWidget'
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from 'scenes/widgets/PostsWidget'
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)")
@@ -17,7 +19,8 @@ const HomePage = () => {
         <UserWidget userId={_id} picturePath={picturePath}/>
       </Box>
       <Box flexBasis={isNonMobileScreens ? "42%": undefined} mt={isNonMobileScreens ? undefined: "2rem"}>
-
+      <MyPostWidget picturePath={picturePath} />
+      <PostsWidget userId={_id} />
       </Box>
       {isNonMobileScreens && 
         <Box flexBasis="26%"></Box>
